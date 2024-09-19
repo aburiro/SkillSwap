c09513273be7afeec20a0423de5ddd736aaf9c23
aburiro <aburiro40@gmail.com>
2024-09-20 01:17:32 +0500

    color Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,9 @@ import '../css_style/Navbar.css';
 
 function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <button className="btn btn-link navbar-brand d-flex align-items-center">
+                <button className="btn btn-link navbar-brand d-flex align-items-center text-white">
                     <img 
                         src="src/assets/imges/skillswap_logo.PNG" 
                         alt="SkillSwap Logo" 
@@ -16,7 +16,7 @@ function Navbar() {
                         height="30" 
                         className="d-inline-block align-top me-2"
                     />
-                    <Link className="navbar-brand mb-0" to="/">SkillSwap</Link>
+                    <Link className="navbar-brand mb-0 text-white" to="/">SkillSwap</Link>
                 </button>
                 <button
                     className="navbar-toggler"
@@ -32,29 +32,29 @@ function Navbar() {
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <NavLink to="/" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+                            <NavLink to="/" className={({ isActive }) => isActive ? "nav-link active text-white" : "nav-link text-white"}>
                                 Home
                             </NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink to="/explore" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+                            <NavLink to="/explore" className={({ isActive }) => isActive ? "nav-link active text-white" : "nav-link text-white"}>
                                 Explore Skills
                             </NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink to="/post" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+                            <NavLink to="/post" className={({ isActive }) => isActive ? "nav-link active text-white" : "nav-link text-white"}>
                                 Post a Skill
                             </NavLink>
                         </li>
                         <li className="nav-item dropdown">
-                            <NavLink to="/account" className={({ isActive }) => isActive ? "nav-link dropdown-toggle active" : "nav-link dropdown-toggle"} id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <NavLink to="/account" className={({ isActive }) => isActive ? "nav-link dropdown-toggle active text-white" : "nav-link dropdown-toggle text-white"} id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 My Account
                             </NavLink>
-                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><Link className="dropdown-item" to="/account/profile">Profile</Link></li>
-                                <li><Link className="dropdown-item" to="/account/skills">My Skills</Link></li>
-                                <li><Link className="dropdown-item" to="/account/settings">Settings</Link></li>
-                                <li><Link className="dropdown-item" to="/logout">Logout</Link></li>
+                            <ul className="dropdown-menu bg-dark" aria-labelledby="navbarDropdown">
+                                <li><Link className="dropdown-item text-white" to="/account/profile">Profile</Link></li>
+                                <li><Link className="dropdown-item text-white" to="/account/skills">My Skills</Link></li>
+                                <li><Link className="dropdown-item text-white" to="/account/settings">Settings</Link></li>
+                                <li><Link className="dropdown-item text-white" to="/logout">Logout</Link></li>
                             </ul>
                         </li>
                     </ul>
